45a4844105642bba876cd1179d32667ac4d2cc04
Adam Silver <adambsilver@gmail.com>
2011-01-06 09:03:34 +0000

    added tests and functionality for params object but not added to route trigger yet, code taken and changed from Sammy js for now

--- tests/Tests.Unit.js
+++ tests/Tests.Unit.js
@@ -124,6 +124,15 @@ test("Converting a route to a regex, should return a correctly formed regex", fu
   //equals(t._routeToRegExp("/woop/:uuid/twooop").toString(), "/^/woop/([^/]+)/twooop/?$/", "The regex returned was correctly replaced.");
 });
 
+module("getParamsFromRoute()")
+
+test("Get params from route returns object with param names as keys", function() {
+  var t = new Tyro();
+  var obj = t.getParamsFromRoute("/setup/:uuid/:whatever", "/setup/123/56d");
+  equals(obj.uuid, "123");
+  equals(obj.whatever, "56d");
+})
+
 module("_triggerRoute()");
 
 test("Triggering a route that does not exist should set the hash to the pageNotFoundUrl (defined in the options property)", function() {
@@ -154,6 +163,15 @@ test("Triggering a route with a particular :param should pass the value to the c
   equals(func2.args[0], "2345abc", "The param was passed to the function.");  
   equals(func2.args[1], "fd54", "The param was passed to the function.");
 });
+
+test("Triggering a route with a particular set of params :param1, :param2 should provide an object with values keyed by param1 and param2", function() {
+  var t = new Tyro();
+  var func1 = stubFn();
+  t.addRoute("/setup/:param1/whatever/:param2/etc", func1);
+  t._triggerRoute("/setup/123/whatever/456/etc");
+  equals(func1.args[0].param1, "123");
+  equals(func1.args[0].param2, "456");
+})
 
 module("_handleHashChange()");
 
