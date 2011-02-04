c2de43e242fe1b4559f0801b1d7d883b454b4c70
adambsilver <adambsilver@gmail.com>
2011-02-04 10:07:31 +0000

    comments

--- src/Tyro/Tyro.PageController.js
+++ src/Tyro/Tyro.PageController.js
@@ -41,22 +41,41 @@ Tyro.PageController = function() {
 	this.activeChildren = [];	
 }
 
+/**
+ * This is the primary method called within other controllers in order to render
+ * and teardown the appropriate partialViews and normal views so that we gain flexibility
+ * in providing "layouts" whilst maintaining a performant code base.
+ * @example
+ * pageController.render({partialViewId: "dashboard", view: "your view object" })
+ * @param {Object} options The options for the render method
+ * @param {String} options.partialViewId The partialViewId required to render before the view is rendered
+ * @param {Boolean} options.special When this option is set to true, it is assumed that the partialView is already rendered
+ * which is useful when a partialView notifies other controllers that it is now rendered, so that a navigation view is shown
+ * but we don't want the partialView to be rerendered or childviews of child partials etc to be torn down
+ * @function
+ * @public
+ * @memberOf Tyro.PageController *
+ */
 Tyro.PageController.prototype.render = function(options) {
-  console.log("PAGECONTROLLER > RENDER");
-
+  console.log("------------------------------PAGECONTROLLER > RENDER");
+	
+	// assume the partialViews parents are already ready
   if(options.special && this.partialViews[options.partialViewId].active) {
-	this.renderView(options.partialViewId, options.view);
-	return;
+		this.renderView(options.partialViewId, options.view);
+		return;
   }
-
+  
+	// if the partial view is already active then we dont need to sort out parents
+	// we only need to sort out the children
   if(this.partialViews[options.partialViewId].active) {
     this.teardownChildPartialViews(options.partialViewId);
   	this.renderView(options.partialViewId, options.view);
   }
+	// if the partial view is not active then we need to do a lot more prep work
   else {
-    console.log("1. teardown non attached parents");
+    //console.log("1. teardown non attached parents");
     this.teardownNonAttachedPartialViews(options.partialViewId);
-    console.log("2. teardown children");
+    //console.log("2. teardown children");
     this.teardownChildPartialViews(options.partialViewId); // children    
     //console.log("3. ensure that the partialView dom node container is not taking the place of any parent partial view child views, teardown if nec");
     var partialViewContainer = this.getPartialViewDomContainer(options.partialViewId);
